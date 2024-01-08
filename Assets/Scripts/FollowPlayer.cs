0c340cbb3ad359db88998038a9b6a6e3988c1e4b
ovarion-official <mvp777999111@gmail.com>
2024-01-08 20:27:46 +0300

    щл

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,7 +6,7 @@ public class FollowPlayer : MonoBehaviour
 {
 
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 offset = new Vector3(2.81, 9.22, -9.9);
 
     void Start()
     {
